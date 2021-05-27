ecc96245df14c9130c6d4079a08791c4d891e2cc
Tyler Correu <60193729+Correu@users.noreply.github.com>
2021-05-26 20:42:01 -0500

    changes to webreader

--- WebReader.py
+++ WebReader.py
@@ -4,6 +4,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 class webReader:
 
@@ -15,7 +16,20 @@ class webReader:
             print(f'{driver.current_url}')
             testtitle = driver.title
             print(f'{testtitle}')
-            test = driver.find_element_by_class_name("s_100")
-            print(f'{test}')
+            #finding the links on each individual page
+            links_List = []
+            findLinks = driver.find_elements(By.PARTIAL_LINK_TEXT, "injury_report")
+            for link in range(len(findLinks)):
+                links_List.append(findLinks[link].text)
+            
+            test = driver.find_elements(By.TAG_NAME, "li")
+            testList = []
+            for i in range(len(test)):
+                testList.append(test[i].text)
+                print(testList[i])
+            listFile = "link_list.txt"
+            with open(listFile, "w") as file_object:
+                for i in range(len(links_List)):
+                    file_object.write(links_List[i])
         finally:
             driver.quit()
